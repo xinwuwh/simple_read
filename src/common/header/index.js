4b8dc189f9eb6266d856baf79e9c8ecef8da6801
XinWu <xinwuwh@163.com>
2019-04-22 23:54:14 +1000

    add CSSTransition

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,9 +1,18 @@
 import React, {Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import {HeaderWrapper,Logo,Nav, NavItem,NavSearch,Addition,Button,SearchWrapper} from './style';
 import { GlobalStyle } from '../../statics/iconfont/iconfont';
 
 
 class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            focused: false
+        }
+        this.handleInputFocus=this.handleInputFocus.bind(this);
+        this.handleInputBlur=this.handleInputBlur.bind(this);
+    }
 
     render(){
         return (
@@ -18,8 +27,21 @@ class Header extends Component{
                     <i className="iconfont">&#xe636;</i>
                     </NavItem>
                     <SearchWrapper>
-                    <NavSearch></NavSearch>
-                    <i className="iconfont">&#xe633;</i>
+                        <CSSTransition
+                        timeout={200}
+                        in={this.state.focused}
+                        classNames="slide"
+                        
+                        >
+                        <NavSearch 
+                        className={this.state.focused ? 'focused' : ''}
+                        onFocus={this.handleInputFocus}
+                        onBlur={this.handleInputBlur}
+                        ></NavSearch>
+                        </CSSTransition>
+                        <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>
+                        &#xe633;</i>
+                        
                     </SearchWrapper>
                     
                 </Nav>
@@ -32,6 +54,16 @@ class Header extends Component{
                 </Addition>
             </HeaderWrapper>
         )
+    }
+    handleInputFocus (){
+        this.setState({
+            focused: true
+        })
+    }
+    handleInputBlur(){
+        this.setState({
+            focused: false
+        })
     }
 }
 
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -53,6 +53,21 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
     float: left;
     position: relative;
+    .slide-enter {
+        width: 160px;
+        transition: all .2s ease-out;
+
+    }
+    .slide-enter-active {
+        width: 240px;
+    }
+    .slide-exit {
+        transition: all .2s ease-out;
+    }
+    .slide-exit-active {
+        width: 160px;
+    }
+
     .iconfont{
         position: absolute;
         right: 5px;
@@ -61,6 +76,10 @@ export const SearchWrapper = styled.div`
         line-height:30px;
         border-radius: 15px;
         text-align: center;
+        &.focused {
+            background: #777;
+            color: #fff;
+        }
     }
 `;
 
@@ -69,7 +88,7 @@ export const NavSearch = styled.input.attrs({
 })`
     width: 160px;
     height: 38px;
-    padding: 0 20px;
+    padding: 0 30px 0 20px;
     margin-top: 9px;
     margin-left: 20px;
     box-sizing: border-box;
@@ -78,8 +97,13 @@ export const NavSearch = styled.input.attrs({
     border-radius: 19px;
     background: #eee;
     font-size: 14 px;
+    color:#666;
     &::placeholder {
         color: #999;
+    }
+    &.focused{
+        width: 240px;
+
     }
 
 `;
