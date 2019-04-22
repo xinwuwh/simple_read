996ec3281290cff507f7d4d7139480b6b34632cc
XinWu <xinwuwh@163.com>
2019-04-22 17:15:44 +1000

    add Header

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
-import React,{ Component, Fragment } from 'react';
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import './style.css'
+import React,{ Component } from 'react';
+import Header from './common/header';
+import { GlobalStyle } from './style';
+
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state={
-      list:[]
-    }
-    this.handleAddItem = this.handleAddItem.bind(this);
-  }
   render(){
       return (
-        <Fragment>
-          <TransitionGroup>
-          {
-            this.state.list.map((item, index)=> {
-              return (
-                <CSSTransition
-                  timeout={1000}
-                  classNames='fade'
-                  unmountOnExit
-                  onEntered={(el) => {el.style.color='blue'}}
-                  appear={true}
-                  key={index}
-                >
-                  <div>{item}</div>
-              </CSSTransition>
-              )
-              })
-          }
-          <button onClick={this.handleAddItem}>toggole</button>
-          </TransitionGroup>
-        </Fragment>
+        <div>
+          <GlobalStyle />
+          <Header />
+        </div>
       
-      
-      )
+      );
   }
 
-  handleAddItem(){
-    this.setState((prevState)=> {
-      return {
-        list: [...prevState.list, 'item']
-      }
-    })
-  }
+  
 }
 export default App;
---
+++ src/common/header/index.js
@@ -0,0 +1,27 @@
+import React, {Component } from 'react';
+import {HeaderWrapper,Logo,Nav, NavItem,NavSearch,Addition,Button} from './style'
+
+class Header extends Component{
+
+    render(){
+        return (
+            <HeaderWrapper>
+                <Logo>Easy Read</Logo>
+                <Nav> 
+                    <NavItem className='left active'>Home</NavItem>
+                    <NavItem className='left'>Download APP</NavItem>
+                    <NavItem className='right'>Sign in</NavItem>
+                    <NavItem className='right'> Aa</NavItem>
+                    <NavSearch></NavSearch>
+                    
+                </Nav>
+                <Addition>   
+                    <Button className='writing'>Writing</Button>
+                    <Button className='reg'>Sign up</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
+    }
+}
+
+export default Header;
