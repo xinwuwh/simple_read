8a5eee1dda90bac9ef88852962e0c25c82b2f162
XinWu <xinwuwh@163.com>
2019-04-24 11:14:20 +1000

    optimise header structure

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
 import {HeaderWrapper,Logo,Nav, NavItem,NavSearch,Addition,Button,SearchWrapper} from './style';
 import { GlobalStyle } from '../../statics/iconfont/iconfont';
 
@@ -57,16 +58,10 @@ const mapStateToProps=(state)=>{
 const mapDispatchToProps=(dispatch)=>{
     return {
         handleInputFocus(){
-            const action={
-                type: 'search_focus'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
-            const action={
-                type: 'search_blur'
-            };
-            dispatch(action);
+            dispatch(actionCreators.searchBlur());
         }
     }
 }
--- src/common/header/store/index.js
+++ src/common/header/store/index.js
@@ -1,3 +1,7 @@
 import reducer from './reducer';
+import * as actionCreators from './actionCreators';
+import * as constants from './constants';
 
-export  {reducer};
+
+
+export  {reducer,actionCreators,constants };
